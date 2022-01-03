980ab6d37a7ef843cb96f83b8ef46903901c36ed
Kyle <plusultra0322@gmail.com>
2022-01-03 13:45:00 +0800

    final

---
+++ drawStraws.js
@@ -0,0 +1,12 @@
+let numberNorth = ["椪柑" , "水蜜桃", "柿子", "草莓"]
+let numberWest = ["葡萄", "琵琶", "橘子", "梨子", "梅子", ] 
+let numberEast = ["李子", "釋迦", "西瓜"]
+let numberSouth = ["芭樂", "芒果", "文旦柚", "蓮霧"]
+
+let srcNorth = ["./picture/椪柑.jpeg", "./picture/水蜜桃1.jpg", "./picture/柿子1.jpg", "./picture/草莓1.jpg"]
+let srcWest = ["./picture/葡萄1.jpg", "./picture/琵琶1.jpg", "./picture/橘子.jpeg", "./picture/梨子1.jpg", "./picture/梅子1.jpg"]
+let srcEast = ["./picture/李子1.jpg", "./picture/釋迦1.jpg", "./picture/西瓜1.jpg"]
+let srcSouth = ["./picture/芭樂1.jpg", "./picture/芒果1.jpg", "./picture/文旦柚1.jpg", "./picture/蓮霧1.jpeg"]
+
+
+
---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#nav li ul").hide();
+    $("#nav li").hover(function() {
+      $("ul", this).slideDown(200);
+      $(this).children("a:first").addClass("hov");
+    } ,function() {
+      $("ul", this).slideUp(100);
+      $(this).children("a:first").removeClass("hov");  
+    })
+  })
